8e0e6ef0fe5fb804c2ebc94752ece5bd940d1d01
tolentinoel <rckrleyn@gmail.com>
2020-10-13 23:11:40 -0700

    just starting

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,30 @@
 class Startup
 
+    attr_reader :founder
+    attr_accessor :name, :domain
+
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_founder(name)
+        @@all.find do |company|
+            company.founder == name
+        end
+    end
+
+    def initialize(name, founder, domain)
+        @name = name
+        @founder = founder
+        @domain = domain
+    end
+
+    def pivot(str_domain, str_name)
+        @domain = str_domain
+        @name = str_name
+    end
+
+
 end
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,6 @@
 class VentureCapitalist
 
+    def initialize(name)
+        @name = name
+    end
 end
