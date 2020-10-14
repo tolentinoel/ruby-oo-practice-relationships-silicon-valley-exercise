848f517797e57069789ebf24cd1b0a986e8b9c54
tolentinoel <rckrleyn@gmail.com>
2020-10-13 23:26:05 -0700

    more things added

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,16 @@
 class FundingRound
 
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    def initialize(startup, v_cap, type)
+        @startup = startup
+        @venture_capitalist = v_cap
+        @type = type
+        @@all << self
+    end
+
 end
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -8,9 +8,21 @@ class VentureCapitalist
         @@all
     end
 
-    def initialize(name)
-        @name = name
+    def self.tres_commas_club
+        self.all.collect {|person| person.total_worth > 1000000000.0}
     end
+
+
+    def initialize(name, moolah)
+        @name = name
+        @money = moolah
+        @@all << self
+    end
+
+    def total_worth
+        @money
+    end
+
 
 
 end
